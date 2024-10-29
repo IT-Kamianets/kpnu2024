2a6c1443ebde3ef08c98d59c31336d11dae19f27
MrachkovskyiOleksandr <147396215+MrachkovskyiOleksandr@users.noreply.github.com>
2024-10-29 12:48:31 +0000

    add data

--- script.js
+++ script.js
@@ -2,31 +2,55 @@ const projectsData = [
   {
     "projectName": "TrustyConvert",
     "projectLink": "https://github.com/IT-Kamianets/trustyconvert",
+    "pageLink": "https://trustyconvert.itkamianets.com/",
     "projectDescription": "Короткий опис проєкту"
   },
   {
     "projectName": "Timer",
     "projectLink": "https://github.com/IT-Kamianets/timer",
+    "pageLink": "https://timer.itkamianets.com/",
     "projectDescription": "Короткий опис проєкту"
   },
   {
     "projectName": "WeatherSense",
     "projectLink": "https://github.com/IT-Kamianets/weathersense",
+    "pageLink": "https://weathersense.itkamianets.com/",
     "projectDescription": "Короткий опис проєкту"
   },
   {
     "projectName": "TodoList",
     "projectLink": "https://github.com/IT-Kamianets/todo",
+    "pageLink": "https://todo.itkamianets.com/",
     "projectDescription": "Короткий опис проєкту"
   },
   {
     "projectName": "CalorieTracker",
     "projectLink": "https://github.com/IT-Kamianets/calorietracker",
+    "pageLink": "http://calorietracker.itkamianets.com/",
     "projectDescription": "Короткий опис проєкту"
   },
   {
     "projectName": "HabitTracker",
     "projectLink": "https://github.com/IT-Kamianets/habittracker",
+    "pageLink": "http://habittracker.itkamianets.com/",
+    "projectDescription": "Короткий опис проєкту"
+  },
+  {
+    "projectName": "RecipiesBook",
+    "projectLink": "https://github.com/IT-Kamianets/recipiesbook",
+    "pageLink": "",
+    "projectDescription": "Короткий опис проєкту"
+  },
+  {
+    "projectName": "Chat App",
+    "projectLink": "https://github.com/IT-Kamianets/chatapp",
+    "pageLink": "https://chatapp.itkamianets.com/",
+    "projectDescription": "Короткий опис проєкту"
+  },
+  {
+    "projectName": "Currency Converter",
+    "projectLink": "https://github.com/IT-Kamianets/currencyconverter",
+    "pageLink": "https://currencyconverter.itkamianets.com/",
     "projectDescription": "Короткий опис проєкту"
   }
 ]
@@ -69,7 +93,8 @@ const studentsData = [
     "profileLink": {
       "gitHub": "https://github.com/izhytskyi",
       "linkedIn": "https://linkedin.com/in/student3",
-      "aboutPage": "https://pavloizhytskyi.itkamianets.com/"
+      "aboutPage": "https://pavloizhytskyi.itkamianets.com/",
+      "telegram": "https://t.me/ixhxtxkxi"
     }
   },
   {
@@ -98,8 +123,9 @@ projectsData.forEach(project => {
   const projectsDiv = document.createElement('div');
   projectsDiv.className = 'project-card';
   projectsDiv.innerHTML = `<h3>${project.projectName}</h3>
-                       <p>${project.projectDescription}</p>
-                       <a href="${project.projectLink}" target="_blank">Перейти до проєкту</a>`;
+          <p>${project.projectDescription}</p>
+          <a href="${project.projectLink}" target="_blank">GiHub проєкту</a>
+          <a href="${project.pageLink}" target="_blank">Сайт проєкту</a>`;
 
   projectsCont.appendChild(projectsDiv);
 });
@@ -108,10 +134,11 @@ studentsData.forEach(student => {
   const studentsDiv = document.createElement('div');
   studentsDiv.className = 'student-card';
   studentsDiv.innerHTML = `<h3>${student.studentName}</h3>
-                         <div class="links">
-                         <a href=${student.profileLink.gitHub} target="_blank">GitHub</a>
-                         <a href=${student.profileLink.linkedIn} target="_blank">LinkedIn</a>
-                         <a href=${student.profileLink.aboutPage} target="_blank">About</a>
-                         </div>`
+          <div class="links">
+          <a href=${student.profileLink.gitHub} target="_blank">GitHub</a>
+          <a href=${student.profileLink.linkedIn} target="_blank">LinkedIn</a>
+          <a href=${student.profileLink.aboutPage} target="_blank">About</a>
+          <a href=${student.profileLink.telegram} target="_blank">Telegram</a>
+          </div>`
   studentsCont.appendChild(studentsDiv);
 })
